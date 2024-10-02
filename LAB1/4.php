4f2da91b75831295e336b7d644bf284d7a7e6374
Mudit Bhatt <154462822+themudit-18@users.noreply.github.com>
2024-10-02 17:20:40 +0530

    BCA Sem - 3 Lab 1 Solution
    It is only for batch  1,2,3 and 4

---
+++ LAB1/4.php
@@ -0,0 +1,10 @@
+<?php
+    $a=10;
+    $b=20;
+    echo "Before Swaping value: a: $a, b: $b";
+    $c = $a;
+    $a = $b;
+    $b = $c;
+    echo "<br/>After Swaping value: a: $a, b: $b";
+      
+?>
---
+++ LAB1/5.php
@@ -0,0 +1,13 @@
+<?php
+    $a = 5;
+    $b = 10;
+    
+    echo "Before swapping: a = $a, b = $b <br>";
+    
+    $a = $a + $b;//a=15
+    $b = $a - $b;//b=5
+    $a = $a - $b;//a=10
+    
+    echo "After swapping: a = $a, b = $b";
+        
+?>
